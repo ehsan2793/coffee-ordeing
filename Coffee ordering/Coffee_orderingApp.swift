a499d7c9812dd7c32c9b420bac979f2ba10fee55
ehsan2793 <ehsan2793@gmail.com>
2024-08-03 22:13:02 -0700

    Initial Commit

---
+++ Coffee ordering/Coffee_orderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_orderingApp.swift
+//  Coffee ordering
+//
+//  Created by Ehsan Rahimi on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_orderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
